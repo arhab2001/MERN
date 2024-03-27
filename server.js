d2b78d542fa6af2f3ae6dc96b919eb2178309a5b
arhab2001 <ararhabahmath2001@gmail.com>
2024-03-27 15:59:11 +0530

    route_files_with_express_router

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+//DefinE Routes
+app.use('/api/user', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
