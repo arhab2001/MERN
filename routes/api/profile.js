2e5784317f343af55250fe59f37410340942dd1f
arhab2001 <ararhabahmath2001@gmail.com>
2024-04-17 20:30:54 +0530

    Create & Update Profile Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -44,6 +44,68 @@ router.post('/', [auth, [
         if(!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            sikkls,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkdin
+        } = req.body;
+
+        // Build profile object
+        const profileFields= {};
+        profileFields.user = red.user.id;
+        if(company) profileFields.company = company;
+        if(website) profileFields.website = website;
+        if(location) profileFields.location = location;
+        if(bio) profileFields.bio = bio;
+        if(status) profileFields.status = status;
+        if(githubusername) profileFields.githubusername = githubusername;
+        if(skills){
+            profileFields.skills = skills.split(',').map(skills => skills.trim());
+        }
+
+        // Build social object
+        profileFields.social = {}
+        if(youtube) profileFields.social.youtube = youtube;
+        if(facebook) profileFields.social.facebook = facebook;
+        if(twitter) profileFields.social.twitter = twitter;
+        if(instagram) profileFields.social.instagram = instagram;
+        if(linkdin) profileFields.social.linkdin = linkdin;
+
+        try{
+            let profile = await Profile.findOne({user: req.user.id});
+
+            if(profile){
+                // Update
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id }, 
+                    { $set: profileFields }, 
+                    {new: true}
+                );
+
+                return res.json(profile);
+            }
+
+            // Create
+            profile = new Profile(profileFields);
+
+            await profile.Save();
+            res.json(profile);
+
+        }   catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+        
     }
 );
 
